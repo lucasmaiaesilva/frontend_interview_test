80825ccaab9954b05b82e322c4f145e2c50ee52e
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2017-09-16 20:59:32 -0300

    Add the components structure

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './components/App.js'
+import App from './components/App'
 
-ReactDOM.render(
+ReactDOM.render (
   <App />,
   document.querySelector('#app')
 )

213e4b8cbc1718d68f6ca329577e3240fe8c3509
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2017-09-16 22:45:37 -0300

    Making the async requests

--- assets/components/App.js
+++ assets/components/App.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const App = () => (
-  <h1>Olá mundo!</h1>
-)
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+  componentDidMount() {
+    axios({
+      method:'get',
+      url:'https://api.mcmakler.de/v1/advertisements'
+    })
+    .then((res) => this.setState({ data: res.data.data}))
+  }
+  render() {
+    console.log(this.state.data)
+    return <h1>Olá mundo!</h1>
+  }
+}
 
 export default App

db93f1b12ecb2867eaa9b445d59abe7196a4ea54
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2017-09-17 15:23:15 -0300

    Removing unused information and optimize the display of the data

--- assets/components/App.js
+++ assets/components/App.js
@@ -14,7 +14,22 @@ class App extends Component {
       method:'get',
       url:'https://api.mcmakler.de/v1/advertisements'
     })
-    .then((res) => this.setState({ data: res.data.data}))
+    .then(res => console.log(this.mountData(res.data.data)))
+  }
+  mountData(obj) {
+    return obj.splice(0, 10).map(house => this.customizeAdv(house))
+  }
+  customizeAdv(item) {
+    const obj = {
+      id: item._id,
+      title: item.title,
+      price: item.advertisementPrice,
+      address: item.realestateSummary.address,
+      rooms: item.realestateSummary.numberOfRooms,
+      space: item.realestateSummary.space,
+      url: item.advertisementAssets[0].advertisementThumbnails.inventory_m.url      
+    }
+    return obj
   }
   render() {
     console.log(this.state.data)
